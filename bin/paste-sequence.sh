697be32a0640dd0ad0b795ee4ec58cfb42620e2a
Surkhab Niazi <niazis@kharon.com>
2023-06-16 09:10:02 -0700

    simple progress bar

--- bin/paste-sequence.sh
+++ bin/paste-sequence.sh
@@ -1,6 +1,9 @@
 array=("${(@f)$(pbpaste)}")
-for i in {1..$#array}; do
-  echo "$i/${#array}: ${array[i]}"
+total=${#array}
+for i in {1..$total}; do
+  completed=$(printf "%0.s#" $(seq 1 $i))
+  remaining=$(printf "%0.s-" $(seq 1 $(($total-$i))))
+  echo "$completed$remaining ${array[i]}"
   echo "${array[i]}" | pbcopy
   read
 done
