2e1228b6f88112d1f41641340b8cb97ec26dcaa9
Surkhab Niazi <niazis@kharon.com>
2023-06-16 09:32:53 -0700

    better progress bar

--- bin/paste-sequence.sh
+++ bin/paste-sequence.sh
@@ -1,8 +1,9 @@
+#!/bin/zsh
 array=("${(@f)$(pbpaste)}")
 total=${#array}
 for i in {1..$total}; do
-  completed=$(printf "%0.s#" $(seq 1 $i))
-  remaining=$(printf "%0.s-" $(seq 1 $(($total-$i))))
+  completed=$(repeat $i printf "-")
+  remaining=$(repeat $(($total-$i)) printf "#")
   echo "$completed$remaining ${array[i]}"
   echo "${array[i]}" | pbcopy
   read
