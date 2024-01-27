5f2ae44c746c903b85cc9387cc5500a1e1e113ed
Surkhab Niazi <niazis@kharon.com>
2024-01-26 17:34:07 -0800

    Corrected the order of revisions if given in the wrong order

--- bin/helmvaluesdiff.sh
+++ bin/helmvaluesdiff.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
 release=${1?please enter release name}
-
 revision=${2}
 compare_revision=${3}
 
 if [ -z "$revision" ]; then
   latestrevisioninfo=$(helm history $release --output json | jq '.[length-1]')
-
-  revision=$( echo $latestrevisioninfo | jq '.revision' )
-  updatedate=$( echo $latestrevisioninfo | jq '.updated')
+  revision=$(echo $latestrevisioninfo | jq '.revision')
+  updatedate=$(echo $latestrevisioninfo | jq '.updated')
 else
   revisioninfo=$(helm history $release --output json | jq ".[] | select(.revision | . == $revision )")
-  updatedate=$( echo $revisioninfo | jq '.updated')
+  updatedate=$(echo $revisioninfo | jq '.updated')
 fi
 
 # Set the previousrevision or use the specified compare_revision
 if [ -z "$compare_revision" ]; then
-previousrevision=$(expr $revision - 1)
+  previousrevision=$(expr $revision - 1)
 else
   previousrevision=$compare_revision
 fi
 
-echo $previousrevision \> $revision updated at $updatedate
+# Determine the order of revisions for diff
+if [ $previousrevision -lt $revision ]; then
+  firstrevision=$previousrevision
+  secondrevision=$revision
+else
+  firstrevision=$revision
+  secondrevision=$previousrevision
+fi
 
-diff <( helm get values $release --all --revision $previousrevision ) <( helm get values $release --all --revision $revision )
+echo Comparing revision $firstrevision with $secondrevision updated at $updatedate
+
+# Perform diff in the correct order
+diff <(helm get values $release --all --revision $firstrevision) <(helm get values $release --all --revision $secondrevision)
