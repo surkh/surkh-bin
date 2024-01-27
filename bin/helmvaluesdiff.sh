53992e39c26a6b04ef803ff7936c7a553f9604b0
Surkhab Niazi <niazis@kharon.com>
2024-01-26 17:36:42 -0800

    show update timestamps for both revisions

--- bin/helmvaluesdiff.sh
+++ bin/helmvaluesdiff.sh
@@ -4,13 +4,16 @@ release=${1?please enter release name}
 revision=${2}
 compare_revision=${3}
 
+# Function to get updated date for a given revision
+get_updated_date() {
+  local rev=$1
+  local revinfo=$(helm history $release --output json | jq ".[] | select(.revision | . == $rev )")
+  echo $(echo $revinfo | jq '.updated')
+}
+
 if [ -z "$revision" ]; then
   latestrevisioninfo=$(helm history $release --output json | jq '.[length-1]')
   revision=$(echo $latestrevisioninfo | jq '.revision')
-  updatedate=$(echo $latestrevisioninfo | jq '.updated')
-else
-  revisioninfo=$(helm history $release --output json | jq ".[] | select(.revision | . == $revision )")
-  updatedate=$(echo $revisioninfo | jq '.updated')
 fi
 
 # Set the previousrevision or use the specified compare_revision
@@ -20,7 +23,7 @@ else
   previousrevision=$compare_revision
 fi
 
-# Determine the order of revisions for diff
+# Determine the order of revisions for diff and get update dates
 if [ $previousrevision -lt $revision ]; then
   firstrevision=$previousrevision
   secondrevision=$revision
@@ -29,7 +32,10 @@ else
   secondrevision=$previousrevision
 fi
 
-echo Comparing revision $firstrevision with $secondrevision updated at $updatedate
+firstrev_update=$(get_updated_date $firstrevision)
+secondrev_update=$(get_updated_date $secondrevision)
+
+echo "Comparing revision $firstrevision (updated at $firstrev_update) with $secondrevision (updated at $secondrev_update)"
 
 # Perform diff in the correct order
 diff <(helm get values $release --all --revision $firstrevision) <(helm get values $release --all --revision $secondrevision)
